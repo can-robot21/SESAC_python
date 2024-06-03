0c5ab17932ad146f15307ecf40ba3944ab917942
can-robot21 <can.robot21@gmail.com>
2024-06-03 20:49:10 +0900

    JScode

---
+++ 04.javascript/hello.js
@@ -0,0 +1,13 @@
+function sayhello() {
+    console.log('Hello');
+}
+
+function sayPrint() {
+    print();
+}
+
+document.write('<H2>헤딩 추가하기</H>')
+
+for (var i = 1; i <= 5; i++) {
+    document.write("<H3>숫자: " + i + "</H3>");
+}
