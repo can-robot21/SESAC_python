4145bf8d9406e3441207e1ba45cdce1214aa3eb2
can-robot21 <can.robot21@gmail.com>
2024-07-03 23:00:12 +0900

    'crm-detail'

--- 00.work/crm/app.py
+++ 00.work/crm/app.py
@@ -9,6 +9,18 @@ PER_PAGE = 20
 START_PAGE = 1
 PER_LIST = 15
 
+def detail(dbname, id):
+    conn = get_db_connect()
+    cur = conn.cursor()
+    
+    query = f'SELECT * FROM {dbname} WHERE id = ?'
+    cur.execute(query, (id,))
+    row = cur.fetchone()
+    columns = [desc[0] for desc in cur.description]  # 컬럼 이름 추출
+    
+    # row 데이터와 컬럼 이름을 딕셔너리 전환
+    info = dict(zip(columns, row))
+    return info
 
 @app.route('/')
 def index():
@@ -54,15 +66,9 @@ def user(page=1):
 
 @app.route('/detail_user/<user_id>')
 def detail_user(user_id):
-    conn = get_db_connect()
-    cur = conn.cursor()
+    user_info = detail("users", user_id)
     
-    query = 'SELECT * FROM users WHERE id = ?'
-    cur.execute(query, (user_id,))
-    row = cur.fetchone()
-    print(f'사용자 ID {user_id}의 검색결과: {row}')
-    
-    return render_template('detail_user.html', user_info=row)
+    return render_template('detail.html', info=user_info)
       
 @app.route('/store')
 @app.route('/store/<int:page>')
@@ -96,6 +102,12 @@ def store(page=1):
     conn.close()
     return render_template('store.html', stores=stores, total_page=total_page, now_page=page, name_query=name_query, start_page=start_page, per_list=PER_LIST, min=min)
 
+@app.route('/detail_store/<store_id>')
+def detail_store(store_id):
+    store_info = detail("stores", store_id)
+    
+    return render_template('detail.html', info=store_info)
+
 @app.route('/item')
 @app.route('/item/<int:page>')
 def item(page=1):
@@ -117,6 +129,12 @@ def item(page=1):
     start_page = ((page - 1) // PER_LIST) * PER_LIST + 1
     
     return render_template('item.html', items=items, total_page=total_page, now_page=page, start_page=start_page, per_list=PER_LIST, min=min)
+
+@app.route('/detail_item/<item_id>')
+def detail_item(item_id):
+    item_info = detail("items", item_id)
+    
+    return render_template('detail.html', info=item_info)
 
 @app.route('/order')
 @app.route('/order/<int:page>')
@@ -140,6 +158,12 @@ def order(page=1):
     
     return render_template('order.html', orders=orders, total_page=total_page, now_page=page, start_page=start_page, per_list=PER_LIST, min=min)
 
+@app.route('/detail_order/<order_id>')
+def detail_order(order_id):
+    order_info = detail("orders", order_id)
+    
+    return render_template('detail.html', info=order_info)
+
 @app.route('/orderitem')
 @app.route('/orderitem/<int:page>')
 def orderitem(page=1):
@@ -161,6 +185,12 @@ def orderitem(page=1):
     start_page = ((page - 1) // PER_LIST) * PER_LIST + 1
         
     return render_template('orderitem.html', orderitems=orderitems, total_page=total_page, now_page=page, start_page=start_page, per_list=PER_LIST, min=min)
+
+@app.route('/detail_orderitem/<orderitem_id>')
+def detail_orderitem(orderitem_id):
+    orderitem_info = detail("orderitems", orderitem_id)
+    
+    return render_template('detail.html', info=orderitem_info)
 
 if __name__ == "__main__":
     create_tables()
